07ac7bb495a16652e5c50353da8b1b3951408873
Tan <tan10tvo@gmail.com>
2024-04-20 10:40:35 -0600

    spelling changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const searchForm = document.getElementById('form');
-const searchInput = document.getElementById('search');
-const resultList = document.getElementById('results');
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const resultsList = document.querySelector('#results');
 
-searchForm.addEventListener('submit', (e) =>{
+searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     searchRecipes();
 })
 
 async function searchRecipes(){
-    const searchValue = searchInput.ariaValueMax.trim();
+    const searchValue = searchInput.value.trim();
     const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=7aa516a5&app_key=dc836a223fb788b11ae390504d9e97ce&from=0&to=10`)
     const data = await response.json();
-    displayRecipes(data.hits)
+    displayRecipes(data.hits);
 }   
 
 function displayRecipes(recipes){
@@ -27,5 +27,6 @@ function displayRecipes(recipes){
         <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
     </div> `
     })
-    resultList.innerHtml = html;
+    resultsList.innerHTML = html;
+
 }
